615ab9682634b8c19ab0cedcfc42ffcc32dad55d
Riri-Z <riri.zamor@gmail.com>
2024-04-26 11:14:27 +0200

    Centralize getData calls

--- src/routes/eco2mix.routes.ts
+++ src/routes/eco2mix.routes.ts
@@ -3,17 +3,13 @@ import {
   createEco2mix,
   getOneByDateHour,
   getDataByDateRange,
-  getLastRecord,
-  getAllEnergiesTrade,
-  getCo2Rate
+  getLastDateRecord
 } from '../eco_2_mix/eco_2_mix_controller';
 
 const eco2mixRoutes = express.Router();
 
 eco2mixRoutes.route('/eco2mix/totalproduction/').get(getDataByDateRange);
-eco2mixRoutes.route('/eco2mix/co2Rate/').get(getCo2Rate);
-eco2mixRoutes.route('/eco2mix/lastRecord').get(getLastRecord);
-eco2mixRoutes.route('/eco2mix/energiesTrade').get(getAllEnergiesTrade);
+eco2mixRoutes.route('/eco2mix/lastDateRecord').get(getLastDateRecord);
 eco2mixRoutes.route('/eco2mix/').get(getOneByDateHour);
 eco2mixRoutes.route('/eco2mix/').post(createEco2mix);
 
