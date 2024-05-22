77ab6d838f1351d9c77397931eaefae212e3e62d
Riri-Z <riri.zamor@gmail.com>
2024-05-22 11:18:44 +0200

    Update DBB, fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import { sequelize } from './db/connection';
-import productRoutes from './routes/products-routes';
 import consommationEnergieRoutes from './routes/consommation_energie-routes';
 import eco2mixRoutes from './routes/eco2mix.routes';
 import cors from 'cors';
 dotenv.config();
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -14,10 +20,9 @@ const port = process.env.PORT || 3000;
 sequelize.sync().then(() => {
   console.log('Database synchronized');
 });
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use(productRoutes);
 app.use(consommationEnergieRoutes);
 app.use(eco2mixRoutes);
 
